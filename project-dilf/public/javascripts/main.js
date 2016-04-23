4d53a6fcfbf234868097fccea217a6be8a0cf81b
Luoana Barbanta <luoanab@gmail.com>
2016-04-23 17:31:43 +0300

    bk stuff

--- project-dilf/public/javascripts/main.js
+++ project-dilf/public/javascripts/main.js
@@ -1,11 +1,4 @@
-// map = new L.Map('map'); 
 
-//  url = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-//  opt = {}
-//  var layer = new L.TileLayer(url, opt); 
-
-//  map.addLayer(layer);
-//  map.setView(new L.LatLng(60.1733244, 24.9410248), 9);
 $(document).ready(function(){
 	var map = L.map('map').setView([ 46.7766092, 23.603842 ], 16);
 
